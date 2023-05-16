f5fc080f1158bbc61ff8bf0f58c3433e086c2bb6
drlsdee <45199822+drlsdee@users.noreply.github.com>
2023-05-16 18:18:24 +0500

    Updated Get-KafkaCatPath
    Now if the file not found, the function returns nothing instead of
    exception

--- Private/Get-KafkaCatPath.ps1
+++ Private/Get-KafkaCatPath.ps1
@@ -4,34 +4,36 @@
 .DESCRIPTION
     The function returns the full path to the kcat/kafkacat binary, if it is present in the current environment.
     The function looks for the file with name "kcat" (which is primary option) or "kafkacat" (last option) in the Kafka home directory.
-    If the file is present, the function returns this path. If not, the function throws an exception.
+    If the file is present, the function returns this path. If not, the function returns nothing.
 .EXAMPLE
     PS C:\> Get-KafkaCatPath
     The function returns the full path to the kcat/kafkacat binary, if present in the current environment.
     Firstly, the function looks for a file named "kcat". If the file is present, the function returns its path.
     If not, the function falls back to the old name, "kafkacat" and looks for this.
-    If neither kcat nor kafkacat were found, the function throws an exception.
+    If neither kcat nor kafkacat were found, the function returns nothing.
 .EXAMPLE
     PS C:\> Get-KafkaCatPath -Name kcat
     The function returns the full path to the kcat/kafkacat binary, if present in the current environment.
     Firstly, the function looks for a file named "kcat". If the file is present, the function returns its path.
     If not, the function falls back to the old name, "kafkacat" and looks for this.
-    If neither kcat nor kafkacat were found, the function throws an exception.
+    If neither kcat nor kafkacat were found, the function returns nothing.
 .EXAMPLE
     PS C:\> Get-KafkaCatPath -Name kafkacat
-    The function returns the full path to the file named "kafkacat", if the file is present in the current environment. If not, the function throws an exception.
+    The function returns the full path to the file named "kafkacat", if the file is present in the current environment. If not, the function returns nothing.
 .INPUTS
     System.String
 .OUTPUTS
     System.String
 #>
-
 function Get-KafkaCatPath {
     [CmdletBinding()]
+    [OutputType([string])]
     param   (
         [Parameter(Position=1)]
         [ValidateSet('kafkacat','kcat')]
-        [string]$Name   =   'kcat'
+        [string]$Name   =   'kcat',
+        [Parameter()]
+        [switch]$Win32
     )
     end     {
         #   Kafka home directory
@@ -41,14 +43,15 @@ function Get-KafkaCatPath {
         #   Append the ".exe" extension for Windows
         #   About this enumeration: see https://learn.microsoft.com/ru-ru/dotnet/api/system.platformid
         #   TL;DR: this is the only valid value for Windows platform on which PowerShell scripts may run
-        if  ([System.Environment]::OSVersion.Platform -eq [System.PlatformID]::Win32NT) {
+        <# if  ([System.Environment]::OSVersion.Platform -eq [System.PlatformID]::Win32NT) {
             [string]$kafkaPath  =   [System.IO.Path]::ChangeExtension($kafkaPath,'.exe')
-        }
+        } #>
+        if  ($Win32)    {   [string]$kafkaPath  =   [System.IO.Path]::ChangeExtension($kafkaPath,'.exe')    }
         #   Test the resulting path and return it, if found:
         if  ([System.IO.File]::Exists($kafkaPath))  {   return  $kafkaPath  }
         #   The file not found; evaluate the "Name" parameter value:
         if  ($Name -ieq 'kafkacat') {   #   The "kafkacat" is the last possible value; if the file not found, it means there are no kcat/kafkacat instances in well-known places
-            throw [System.IO.FileNotFoundException]::new('The file not found!', $kafkaPath)
+            return
         }
         #   Call this function recursively
         Get-KafkaCatPath -Name kafkacat -ErrorAction Stop
