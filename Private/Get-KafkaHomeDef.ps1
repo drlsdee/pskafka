92a5c275f213d4306ed1545ecef182196b8c4bad
drlsdee <45199822+drlsdee@users.noreply.github.com>
2023-05-16 13:34:13 +0500

    Added helper functions
    - Get-KafkaHomeDef - returns an input string if the path exists;
    - Get-KafkaHomeEnv - returns the value of the KAFKA_HOME if the path
    exists;
    - Get-KafkaHomeCmd - returns the parent path for installed kcat or
    kafkacat.

---
+++ Private/Get-KafkaHomeDef.ps1
@@ -0,0 +1,45 @@
+﻿<#
+.SYNOPSIS
+    The function gets an input string and returns it, if the string is a valid path.
+.DESCRIPTION
+    The function gets an input string and returns it, if the string is a valid path.
+.EXAMPLE
+    PS C:\> Get-KafkaHomeDef
+    The function returns nothing.
+.EXAMPLE
+    PS C:\> Get-KafkaHomeDef -Default ".\default\kafka\path"
+    If the specified path exists, the function returns it. Otherwise it returns nothing.
+.INPUTS
+    System.String
+.OUTPUTS
+    System.String
+#>
+
+function Get-KafkaHomeDef {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param   (
+        [Parameter(Position=1)]
+        [AllowNull()]
+        [AllowEmptyString()]
+        [string]
+        $Default
+    )
+    end     {
+        #   Resolve full path
+        try {
+            $Default    =   Resolve-Path -Path $Default -ErrorAction Stop
+        }
+        catch {
+            $Default    =   $null
+        }
+        #   The string is empty; return nothing:
+        if  ([string]::IsNullOrEmpty($Default) -or [string]::IsNullOrWhiteSpace($Default))  {   return  }
+        #   The directory exists; return the path:
+        if  ([System.IO.Directory]::Exists($Default))   {   return  $Default    }
+        #   The file exists; return the parent path:
+        if  ([System.IO.File]::Exists($Default))   {   return  [System.IO.FileInfo]::new($Default).DirectoryName    }
+        #   Neither file nor directory exists; return nothing:
+        return
+    }
+}
---
+++ Private/Get-KafkaHomeEnv.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS
+    The function gets a value of the "KAFKA_HOME" environment variable and returns it, if the path is valid. Otherwise it returns nothing.
+.DESCRIPTION
+    The function gets a value of the "KAFKA_HOME" environment variable and returns it, if the path is valid. Otherwise it returns nothing.
+.EXAMPLE
+    PS C:\> Get-KafkaHomeEnv
+    The function gets a value of the environment variable "KAFKA_HOME" and returns it, if the path is valid. Otherwise it returns nothing.
+.OUTPUTS
+    System.String
+#>
+
+function Get-KafkaHomeEnv {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param   ()
+    end     {
+        #   Get the environment variable "KAFKA_HOME":
+        [string]$outputPath =   [System.Environment]::GetEnvironmentVariable('KAFKA_HOME')
+        #   The variable does not exist or contains an empty string; return nothing:
+        if  ([string]::IsNullOrEmpty($outputPath) -or [string]::IsNullOrWhiteSpace($outputPath))    {   return  }
+        #   The variable contains a valid directory path; return it immediately:
+        if  ([System.IO.Directory]::Exists($outputPath))    {   return  $outputPath }
+        #   The variable contains a valid FILE path; return the parent directory path:
+        if  ([System.IO.File]::Exists($outputPath)) {   return  [System.IO.FileInfo]::new($outputPath).DirectoryName    }
+        #   The variable contains a path that does not exist; return nothing:
+        return
+    }
+}
